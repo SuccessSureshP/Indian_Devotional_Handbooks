2370127a559a53e0c6f979adcfcc3bf56065f402
provsureshpokkuluri <Suresh.Pokkuluri@providence.org>
2019-04-01 18:25:24 -0700

    Added App Icon and Splash screen

---
+++ app/src/main/java/com/example/suresh/indian_devotional_handbooks/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.example.suresh.indian_devotional_handbooks
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+
+
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val intent = Intent(this, DevotionalHandBooksListActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
